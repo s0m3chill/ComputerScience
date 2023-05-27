ffa339c96bf8b253567722ea057299dab2928f67
Dariy <d.kordiyak@gmail.com>
2023-05-27 09:27:27 +0300

    Add classic BinarySearch

---
+++ BinarySearch.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+
+public class BinarySearch {
+    
+    func binarySearch(sortedArray: [Int], target: Int) -> Int {
+        var left = 0, right = sortedArray.count
+        
+        while (right - left > 1) {
+            let mid = left + (right - left) / 2
+            
+            if sortedArray[mid] > target {
+                right = mid
+            } else {
+                left = mid
+            }
+        }
+        
+        return sortedArray[left] == target ? left : -1
+    }
+    
+}
+
+/// Usage:
+///
+///
+let solution = BinarySearch()
+let targetIndex = solution.binarySearch(sortedArray: [2,4,7,8,10,15], target: 4)
